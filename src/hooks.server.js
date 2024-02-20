46721028b2a7eceaf446cc876d8e55c6b2f80a70
Matt Pilott <hello@matt-pilott.com>
2024-02-20 11:18:34 +0000

    fix quotes on csp self

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -14,7 +14,7 @@ export const handle = sequence(async ({ event, resolve }) => {
 	const response = await resolve(event)
 
 	response.headers.set('Cache-Control', 'no-cache')
-	response.headers.set('Content-Security-Policy', 'frame-ancestors "self"')
+	response.headers.set('Content-Security-Policy', "frame-ancestors 'self'")
 	response.headers.set('Permissions-Policy', 'fullscreen=*')
 	response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
 	response.headers.set('X-Content-Type-Options', 'nosniff')
